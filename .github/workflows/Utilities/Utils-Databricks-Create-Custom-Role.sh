6c042cafd3c580f27569f184559f051beb12b59b
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-09-21 13:36:47 +0000

    test

--- .github/workflows/Utilities/Utils-Databricks-Create-Custom-Role.sh
+++ .github/workflows/Utilities/Utils-Databricks-Create-Custom-Role.sh
@@ -4,11 +4,11 @@ Bool_Contains_DBX_Custom_Role_Exists=$( jq -r  ' [ .[].roleName | contains("DBX_
 echo "Does Custome Role Exist: $Bool_Contains_DBX_Custom_Role_Exists "
 
 if [[ ! $Bool_Contains_DBX_Custom_Role_Exists ]]; then
-
+    echo "Is it..."
     cd .github/workflows/RBAC_Role_Definition
     ls
 
     updateJson="$(jq --arg param_SubscriptionId "$param_SubscriptionId" '.assignableScopes[0] = "/subscriptions/$param_SubscriptionId"' DBX_Custom_Role.json)" && echo -E "${contents}" > DBX_Custom_Role.json
     az role definition create \
-        --role-definition "DBX_Custom_Role.json"
+        --role-definition $updateJson
 fi
