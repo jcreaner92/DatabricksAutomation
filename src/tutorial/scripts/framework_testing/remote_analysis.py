1aedd401ed577f15d6bce8c2434b96369ab353e0
ciaran28 <ciaran28>
2022-09-22 12:50:16 +0000

    Demo

--- src/tutorial/scripts/framework_testing/remote_analysis.py
+++ src/tutorial/scripts/framework_testing/remote_analysis.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 """Example of framework usage"""
-# If setting up scope with Keyvault --> Ensure you have RBAC to KV Admin From 'AzureDatabrics' to Key Vault.
+# If setting up scope with Keyvault --> Ensure you have RBAC to KV Admin From 'AzureDatabrics' to Key Vault. Test
 import random
 from acai_ml.core import Engine
 import pandas as pd
